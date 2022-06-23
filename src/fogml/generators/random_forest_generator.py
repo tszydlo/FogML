43d8e7a871702757654b6bf21f4c2d0bcb980fdc
tszydlo <tszydlo@agh.edu.pl>
2022-06-23 15:05:56 +0200

    Corrections in decision forrest.

--- src/fogml/generators/random_forest_generator.py
+++ src/fogml/generators/random_forest_generator.py
@@ -42,7 +42,7 @@ class RandomForestCodeGenerator(BaseGenerator):
 
     def generate(self, fname = 'random_forest_model.c', cname="classifier", **kwargs):
         result = self.license_header()
-        result += "int %s(double * x){\n" % cname
+        result += "int %s(float * x){\n" % cname
         result += "  int results[%s];\n" % len(self.clf.estimators_)
         index = 0
         for estimator in self.clf.estimators_:
@@ -62,6 +62,7 @@ class RandomForestCodeGenerator(BaseGenerator):
         result += "  		result_class = i;\n"
         result += "  	}\n"
         result += "  }\n"
+        result += "  return result_class;\n"
         result += "}\n"
         with open(fname, 'w') as c_file:
             c_file.write(result)
