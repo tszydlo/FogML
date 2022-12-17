30c57d20df0e0dc35c198f1f2f56e0227aa5c165
pwgacek <pawelgacek.pl@gmail.com>
2022-12-17 20:34:16 +0100

    modified knn_skeleton, c code almost works

--- src/fogml/generators/knn_generator.py
+++ src/fogml/generators/knn_generator.py
@@ -15,24 +15,23 @@ class KNClassifierGenerator(BaseGenerator):
         self.clf = clf
 
     def generate(self, fname = "k_neighbors.c", cname="classifier", **kwargs):
+
+        samples_num = self.clf.n_samples_fit_
+        features_num = self.clf.n_features_in_
+
+
         X = self.clf._fit_X
         X = X.astype(str)
-        result = "float X[][] = [[" + "],[".join([",".join(row) for row in X]) + "]]\n"
 
         y = self.clf._y
         y = y.astype(str)
-        result += "float y[] = [" + ",".join(y) + "]"
 
-        X_str = "[[" + "],[".join([",".join(row) for row in X]) + "]]"
-        y_str = "[" + ",".join(y) + "]"
+        X_str = "{{" + "},{".join([",".join(row) for row in X]) + "}}"
+        y_str = "{" + ",".join(y) + "}"
         classes_str = self.clf.classes_.astype(str)
-        classes_str = "[" + ",".join(classes_str) + "]"
-        # print(result)
-        # with open(fname, 'w') as c_file:
-        #     c_file.write(result)
-        samples_num = self.clf.n_samples_fit_
-        features_num = self.clf.n_features_in_
-        print("hi")
+        classes_str = "{" + ",".join(classes_str) + "}"
+
+
         with open(os.path.join(os.path.dirname(__file__), self.skeleton_path)) as skeleton:
             code = skeleton.read()
             code = self.license_header() + code
@@ -40,10 +39,7 @@ class KNClassifierGenerator(BaseGenerator):
             code = code.replace('<features>', str(features_num))
             code = code.replace('<n_neighbors>', str(self.clf.n_neighbors))
             code = code.replace('<cname>', cname)
-            code = code.replace('<X_train>', X_str)
-            code = code.replace('<Y_train>', y_str)
-            code = code.replace('<n_classes>', str(len(self.clf.classes_)))
-            code = code.replace('<classes>', classes_str)
+            code = code.replace('<train_data>', X_str)
 
 
 
