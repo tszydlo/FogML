660703be0d76be829549e07ebe053d8285a280d8
pwgacek <pawelgacek.pl@gmail.com>
2022-12-17 12:06:53 +0100

    added knn_skeleton & generating simple .c file

--- src/fogml/generators/knn_generator.py
+++ src/fogml/generators/knn_generator.py
@@ -1,8 +1,13 @@
+import os
+
 from .base_generator import BaseGenerator
 from sklearn.neighbors import KNeighborsClassifier
 
 
 class KNClassifierGenerator(BaseGenerator):
+
+    skeleton_path = 'skeletons/knn_skeleton.txt'
+
     def __init__(self, clf: KNeighborsClassifier):
         if not isinstance(clf, KNeighborsClassifier):
             raise ValueError("Expected instance of KNeighborsClassifier")
@@ -18,8 +23,33 @@ class KNClassifierGenerator(BaseGenerator):
         y = y.astype(str)
         result += "float y[] = [" + ",".join(y) + "]"
 
-        print(result)
+        X_str = "[[" + "],[".join([",".join(row) for row in X]) + "]]"
+        y_str = "[" + ",".join(y) + "]"
+        classes_str = self.clf.classes_.astype(str)
+        classes_str = "[" + ",".join(classes_str) + "]"
+        # print(result)
         # with open(fname, 'w') as c_file:
         #     c_file.write(result)
+        samples_num = self.clf.n_samples_fit_
+        features_num = self.clf.n_features_in_
+        print("hi")
+        with open(os.path.join(os.path.dirname(__file__), self.skeleton_path)) as skeleton:
+            code = skeleton.read()
+            code = self.license_header() + code
+            code = code.replace('<n_samples>', str(samples_num))
+            code = code.replace('<features>', str(features_num))
+            code = code.replace('<n_neighbors>', str(self.clf.n_neighbors))
+            code = code.replace('<cname>', cname)
+            code = code.replace('<X_train>', X_str)
+            code = code.replace('<Y_train>', y_str)
+            code = code.replace('<n_classes>', str(len(self.clf.classes_)))
+            code = code.replace('<classes>', classes_str)
+
+
+
+
+
+            with open(fname, 'w') as output_file:
+                output_file.write(code)
     
     
